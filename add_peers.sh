ee7c78e5daed14ab6fd2f5402d9e612d2e2751e1
smk762 <smk762@iinet.net.au>
2023-06-21 03:10:34 +0800

    make confs accessible by host

---
+++ add_peers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# In case you are having trouble getting chains to sync, you can try addding these peers, or add your own manually.
+
+# TODO: Iterate over main coin list and addnode the peer list for notaries.
