f9c5ffea8cd092999bea9eac942f4e26fcaca642
smk762 <smk762@iinet.net.au>
2023-06-22 18:07:40 +0800

    create launch files on setup

--- setup_main.sh
+++ setup_main.sh
@@ -39,3 +39,6 @@ sudo ln -s /home/$USER/.litecoin/litecoin-cli /usr/local/bin/litecoin-cli
 ~/dPoW/iguana/listassetchains | while read coin; do
     sudo ln -s /home/$USER/.komodo/${coin}/${coin}-cli /usr/local/bin/${coin}-cli
 done
+
+echo "Setting up launch files..."
+./configure.py launch
