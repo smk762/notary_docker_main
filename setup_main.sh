4ed49079f9988f6ae25420185266a5d4bba293cc
smk762 <smk762@iinet.net.au>
2023-06-22 17:23:44 +0800

    symlink clis on setup

--- setup_main.sh
+++ setup_main.sh
@@ -29,6 +29,13 @@ echo "" > /home/$USER/.litecoin/debug.log
     echo "" > /home/$USER/.komodo/${coin}/debug.log
 done
 
-
 echo "Building docker images..."
 docker compose build
+
+# Initialising cli binaries log files
+./configure.py clis
+sudo ln -s /home/$USER/.komodo/komodo-cli /usr/local/bin/komodo-cli
+sudo ln -s /home/$USER/.litecoin/litecoin-cli /usr/local/bin/litecoin-cli
+~/dPoW/iguana/listassetchains | while read coin; do
+    sudo ln -s /home/$USER/.komodo/${coin}/${coin}-cli /usr/local/bin/${coin}-cli
+done
