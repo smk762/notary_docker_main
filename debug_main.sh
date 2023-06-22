4dcaabc5f24d52dd3e12bcfd45f79494c8ed864d
smk762 <smk762@iinet.net.au>
2023-06-22 16:27:58 +0800

    add debug script

---
+++ debug_main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z ${1} ]]; then
+    read -p "Enter coin ticker: " coin
+else
+    coin=$1
+fi
+
+if [[ "$coin" == "KMD" ]]
+    then
+    tail -f ~/.komodo/debug.log
+    else
+    tail -f ~/.komodo/${coin}/debug.log
+fi
